78396d28a15369a9b9a1bdb0ab23f94b16befce6
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-02-22 20:56:39 +0300

    add solution system linux level01 task 17

---
+++ src/ru/javarush/system/linux/level01/task17/solution.sh
@@ -0,0 +1,13 @@
+# Создаем директорию task1 в домашней директории
+mkdir -p ~/task1
+
+# Переходим в созданную директорию
+cd ~/task1
+
+# Создаем пустой файл testfile.txt
+touch testfile.txt
+
+# Добавляем текст "Hello, Linux World!" в файл с помощью редактора nano
+
+# Выводим содержимое файла testfile.txt на экран
+cat testfile.txt
