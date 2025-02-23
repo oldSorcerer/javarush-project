000054776b7b1d5ccb9e04df9b5216755d93ad2f
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-02-23 15:16:22 +0300

    add solution system linux level01 task 04

--- src/ru/javarush/system/linux/level01/task04/solution.sh
+++ src/ru/javarush/system/linux/level01/task04/solution.sh
@@ -1,7 +1,8 @@
 # Создание директории example_directory
-
+mkdir "example_directory"
 
 # Перемещение в директорию example_directory
-
+cd example_directory
 
 # Показ содержимого текущей директории
+ls
