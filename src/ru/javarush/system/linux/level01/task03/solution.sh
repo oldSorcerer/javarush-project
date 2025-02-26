101738ff4a16c33540d0cefac159953c614db9de
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-02-26 16:56:04 +0300

    add solution system linux level01 task 03

--- src/ru/javarush/system/linux/level01/task03/solution.sh
+++ src/ru/javarush/system/linux/level01/task03/solution.sh
@@ -1,10 +1,11 @@
 # Создание директории example в текущей рабочей директории
-
+mkdir example
 
 # Переход в созданную директорию example
-
+cd example
 
 # Создание директории project внутри example
-
+mkdir project
 
 # Переход в созданную директорию project
+cd project
