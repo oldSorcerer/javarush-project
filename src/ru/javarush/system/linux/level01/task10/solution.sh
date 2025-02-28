7e15ac12f317d61d22504761bd6749c601a5cd7c
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-02-28 17:06:36 +0300

    add solution system linux level01 task 10

--- src/ru/javarush/system/linux/level01/task10/solution.sh
+++ src/ru/javarush/system/linux/level01/task10/solution.sh
@@ -1,10 +1,11 @@
 # Создаём папку temp
-
+mkdir temp
 
 # Создаём файл temporary.txt внутри папки temp
-
+touch temp/temporary.txt
 
 # Удаляем файл temporary.txt
-
+rm temp/temporary.txt
 
 # Удаляем папку temp
+rm -r temp
