23a32b6ecf99d47bf6a14fcf3e68739618410afc
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-02-28 23:50:46 +0300

    add solution system linux level01 task 12

--- src/ru/javarush/system/linux/level01/task12/solution.sh
+++ src/ru/javarush/system/linux/level01/task12/solution.sh
@@ -1,10 +1,15 @@
 # Создание структуры директорий
-
+mkdir -p website/assets/css
+mkdir website/assets/js
+mkdir website/content
 
 # Создание файлов
-
+touch website/index.html
+touch website/assets/css/styles.css
+touch website/assets/js/script.js
 
 # Копирование папки assets в content
-
+cp -r website/assets website/content
 
 # Удаление папки js из assets
+rm -r website/assets/js
