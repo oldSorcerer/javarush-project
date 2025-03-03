08d939ca6aa6274e841dbe50a0108fb2a131f553
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-03-03 17:43:25 +0300

    add solution system linux level01 task 20

--- src/ru/javarush/system/linux/level01/task20/solution.sh
+++ src/ru/javarush/system/linux/level01/task20/solution.sh
@@ -1,22 +1,23 @@
 # Создаем директорию task4 в домашней директории
-
+mkdir ~/task4
 
 # Переходим в созданную директорию
-
+cd ~/task4
 
 # Создаем текстовый файл testfile.txt и добавляем в него текст
-
+echo "Hello, Linux World!" > testfile.txt
 
 # Создаем поддиректорию backup
-
+mkdir backup
 
 # Копируем файл testfile.txt в поддиректорию backup и называем его backup_testfile.txt
-
+cp testfile.txt backup/backup_testfile.txt
 
 # Удаляем файл testfile.txt из текущей директории
-
+rm testfile.txt
 
 # Восстанавливаем файл, копируя из поддиректории backup обратно
-
+cp backup/backup_testfile.txt testfile.txt
 
 # Проверяем содержимое восстановленного файла
+cat testfile.txt
