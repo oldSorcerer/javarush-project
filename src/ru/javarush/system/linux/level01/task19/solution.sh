d5142dcad1b3e4e5dc095234ddfec443e28e2657
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-03-03 17:42:56 +0300

    add solution system linux level01 task 19

--- src/ru/javarush/system/linux/level01/task19/solution.sh
+++ src/ru/javarush/system/linux/level01/task19/solution.sh
@@ -1,19 +1,21 @@
 # 1. Создание директории task3 в домашней директории
-
+mkdir ~/task3
 
 # 2. Переход в созданную директорию
-
+cd ~/task3
 
 # 3. Создание текстового файла testfile.txt с текстом "Hello, Linux World!"
-
+echo "Hello, Linux World!" > testfile.txt
 
 # 4. Создание поддиректории backup внутри task3
-
+mkdir backup/
 
 # 5. Копирование файла testfile.txt в поддиректорию backup с новым именем backup_testfile.txt
-
+cp testfile.txt backup/backup_testfile.txt
 
 # 6. Проверка наличия файла testfile.txt в текущей директории
-
+ls
 
 # 7. Проверка наличия файла backup_testfile.txt в поддиректории backup
+cd backup
+ls
