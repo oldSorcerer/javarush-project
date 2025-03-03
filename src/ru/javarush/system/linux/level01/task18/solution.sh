b5554ce741614e0e7fedcc95c327d2b25ed2a6e5
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-03-03 17:42:38 +0300

    add solution system linux level01 task 18

--- src/ru/javarush/system/linux/level01/task18/solution.sh
+++ src/ru/javarush/system/linux/level01/task18/solution.sh
@@ -1,16 +1,18 @@
 # Создаем директорию task2 в домашней директории
-
+mkdir ~/task2
 
 # Переходим в созданную директорию
-
+cd ~/task2
 
 # Создаем файл testfile.txt и добавляем в него текст
-
+echo "Hello, Linux World!" > testfile.txt
 
 # Копируем файл testfile.txt в backup_testfile.txt
-
+cp testfile.txt backup_testfile.txt
 
 # Отображаем содержимое текущей директории
-
+ls
 
 # Выводим содержимое файла backup_testfile.txt
+cat backup_testfile.txt
+
