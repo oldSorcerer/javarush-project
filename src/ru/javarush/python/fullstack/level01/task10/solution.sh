71c9dc273b5d5b01522ca9cc606a952bc5e98811
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-03-04 15:21:44 +0300

    add solution docker level01 task 10

---
+++ src/ru/javarush/python/fullstack/level01/task10/solution.sh
@@ -0,0 +1,7 @@
+# Загрузка образа nginx с тегом latest из Docker Hub
+
+# Построение образа из Dockerfile в текущем каталоге с тегом my_custom_image
+
+# Вывод списка локальных образов Docker
+
+# Удаление образа my_custom_image
