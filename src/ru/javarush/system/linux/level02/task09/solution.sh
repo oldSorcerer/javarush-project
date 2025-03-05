ccb0c45f7136c1a7f57ab2305111e02071867f82
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-03-05 13:16:44 +0300

    add solution system linux level02 task 09

--- src/ru/javarush/system/linux/level02/task09/solution.sh
+++ src/ru/javarush/system/linux/level02/task09/solution.sh
@@ -1,5 +1,5 @@
 # Создаем новую группу с именем team_alpha с использованием команды groupadd
-
+sudo groupadd team_alpha
 
 # Проверяем, что группа была добавлена успешно. Для этого выводим информацию о группах и ищем "team_alpha".
 getent group | grep team_alpha
