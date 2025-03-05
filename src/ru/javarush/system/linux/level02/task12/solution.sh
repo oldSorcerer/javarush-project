c9bf5744e69767b1b62acb53f809eae2d0cb955f
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-03-05 13:17:01 +0300

    add solution system linux level02 task 12

--- src/ru/javarush/system/linux/level02/task12/solution.sh
+++ src/ru/javarush/system/linux/level02/task12/solution.sh
@@ -1,16 +1,18 @@
 # Создание группы `design_team`
-
+sudo groupadd design_team
 
 # Создание пользователя `john`
-
+sudo adduser john
 
 # Добавление пользователя `john` в группу `design_team`
-
+sudo usermod -aG design_team john
 
 # Удаление пользователя `john` из группы `design_team`
-
+sudo gpasswd -d john design_team
 
 # Удаление созданной группы `design_team`
-
+sudo groupdel design_team
 
 # Проверка, что группы `design_team` больше не существует
+getent group
+#cat /etc/group
