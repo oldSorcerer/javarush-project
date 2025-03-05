6edd4049e55ad29a25c6b1398b802c6e1ab20de1
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-03-05 13:16:30 +0300

    add solution system linux level02 task 07

--- src/ru/javarush/system/linux/level02/task07/solution.sh
+++ src/ru/javarush/system/linux/level02/task07/solution.sh
@@ -2,7 +2,7 @@
 adduser tester_user
 
 # Смена shell для пользователя tester_user на /bin/zsh с использованием команды usermod
-sudo
+sudo usermod --shell /bin/zsh tester_user
 
 # Проверка, изменен ли shell пользователя путем поиска строки в файле /etc/passwd
 grep -q "^tester_user:.*:/bin/zsh$" /etc/passwd
