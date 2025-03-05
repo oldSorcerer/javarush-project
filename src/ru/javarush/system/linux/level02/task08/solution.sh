35543aff24c1acbd52b013176dff28a436a8ef99
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-03-05 13:16:39 +0300

    add solution system linux level02 task 08

--- src/ru/javarush/system/linux/level02/task08/solution.sh
+++ src/ru/javarush/system/linux/level02/task08/solution.sh
@@ -1,7 +1,8 @@
 # Устанавливаем новый пароль для пользователя developer
-
+sudo passwd developer
 
 # Принуждаем пользователя сменить пароль при следующем входе
-
+sudo passwd -e developer
 
 # Проверяем параметры пользователя
+sudo chage -l developer
