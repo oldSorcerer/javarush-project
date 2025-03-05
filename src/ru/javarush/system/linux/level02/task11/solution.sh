7f3c086fc4d9beff6d5f147769ea5631685c161f
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-03-05 13:16:56 +0300

    add solution system linux level02 task 11

--- src/ru/javarush/system/linux/level02/task11/solution.sh
+++ src/ru/javarush/system/linux/level02/task11/solution.sh
@@ -1,19 +1,20 @@
 # Создание группы project_team
-
+sudo groupadd project_team
 
 # Создание пользователя mike
-
+sudo useradd mike
 
 # Создание пользователя sara
-
+sudo useradd sara
 
 # Добавление mike в группу project_team
-
+sudo usermod -aG project_team mike
 
 # Добавление sara в группу project_team
-
+sudo usermod -aG project_team sara
 
 # Вывод информации о принадлежности mike к группам
-
+groups mike
 
 # Вывод информации о принадлежности sara к группам
+groups sara
