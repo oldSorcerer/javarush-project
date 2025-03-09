ee002fa4f02f93159e619faefad71f63b37bffda
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-03-09 10:28:48 +0300

    add solution system linux level02 task 04

--- src/ru/javarush/system/linux/level02/task04/solution.sh
+++ src/ru/javarush/system/linux/level02/task04/solution.sh
@@ -1,17 +1,20 @@
 # Устанавливаем маску доступа 0022
-
+umask 0022
 
 # Создаем директорию task4_dir
-
+mkdir task4_dir
 
 # Создаем файл task4.txt внутри директории task4_dir
-
+touch task4_dir/task4.txt
 
 # Устанавливаем права доступа для task4.txt
 # Владельцу - чтение и выполнение (rx), группе - выполнение (x), остальным - чтение (r)
-
+chmod 514 task4_dir/task4.txt
 
 # Смена владельца файла task4.txt на root и группы на sudo
-
+sudo chown root:sudo task4_dir/task4.txt
 
 # Проверяем права доступа, владельца и группу файла task4.txt
+ls -l task4_dir/task4.txt
+
+sleep 10
