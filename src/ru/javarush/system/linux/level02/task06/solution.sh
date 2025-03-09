a8595cdea00d8bfcdd0c18fd9bf93aaf5747c0ee
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-03-09 12:09:48 +0300

    add solution system linux level02 task 06

--- src/ru/javarush/system/linux/level02/task06/solution.sh
+++ src/ru/javarush/system/linux/level02/task06/solution.sh
@@ -1,8 +1,8 @@
 # Измените имя пользователя `new_user` на `developer`.
-
+sudo usermod -l developer new_user
 
 # Смените домашнюю директорию пользователя на `/home/developer` с переносом всех данных.
-
+sudo usermod -d /home/developer -m developer
 
 # Визуально проверьте изменения в файле `/etc/passwd`.
 grep developer /etc/passwd
