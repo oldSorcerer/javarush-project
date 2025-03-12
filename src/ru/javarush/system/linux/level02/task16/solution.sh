f4eefe7dfbdb999279d92708e48be8b556b0e830
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-03-12 13:18:30 +0300

    add solution system linux level02 task 16

--- src/ru/javarush/system/linux/level02/task16/solution.sh
+++ src/ru/javarush/system/linux/level02/task16/solution.sh
@@ -1,10 +1,11 @@
 # Создание новой группы с именем testgroup
-
+sudo groupadd testgroup
 
 # Добавление текущего пользователя в группу testgroup
-
+sudo usermod -aG testgroup $(whoami)
 
 # Проверка обновленного списка групп для текущего пользователя
-
+sudo groups $(whoami)
 
 # Подтверждение принадлежности к новой группе
+sudo id $(whoami)
