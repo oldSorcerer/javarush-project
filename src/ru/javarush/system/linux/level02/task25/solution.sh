51299011cfc270b0f3ae7d2c5d541cb72688ce1a
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-04-07 15:52:59 +0300

    add solution system linux level02 task 25

--- src/ru/javarush/system/linux/level02/task25/solution.sh
+++ src/ru/javarush/system/linux/level02/task25/solution.sh
@@ -1,11 +1,12 @@
 # Создаем первого пользователя user1
-
+sudo adduser --disabled-password --gecos "" user1
 
 # Создаем второго пользователя user2
-
+sudo adduser --disabled-password --gecos "" user2
 
 # Проверяем, существуют ли домашние директории для пользователей
-
+ls /home
 
 # Проверяем права собственности на домашние директории
-
+ls -ld /home/user1
+ls -ld /home/user2
