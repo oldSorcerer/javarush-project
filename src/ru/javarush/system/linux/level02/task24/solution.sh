8606b141bf5fc462931100a65a6004a3d8184dd6
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-04-07 15:52:53 +0300

    add solution system linux level02 task 24

--- src/ru/javarush/system/linux/level02/task24/solution.sh
+++ src/ru/javarush/system/linux/level02/task24/solution.sh
@@ -1,7 +1,15 @@
 # Создаём нового пользователя с помощью команды adduser
-
+sudo adduser new_user
 
 # Выполняем вход от имени нового пользователя и запускаем фоновые процессы
+#sudo su new_user
+#sleep 800 &
+#sleep 900 &
+#exit
 
+su - new_user -c "sleep 800 &"
+su - new_user -c "sleep 900 &"
 
 # Завершаем все процессы, принадлежащие новому пользователю
+sudo pkill -u new_user
+ps aux | grep sleep
