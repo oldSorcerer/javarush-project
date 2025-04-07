22bf747c5d7e34034e8f3e1997fda7061e56af7f
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-04-07 15:53:06 +0300

    add solution system linux level02 task 26

--- src/ru/javarush/system/linux/level02/task26/solution.sh
+++ src/ru/javarush/system/linux/level02/task26/solution.sh
@@ -1,11 +1,13 @@
 # Создание группы "developers"
-
+sudo groupadd developers
 
 # Добавление пользователей `user1` и `user2` в группу "developers"
-
+sudo usermod -aG developers user1
+sudo usermod -aG developers user2
 
 # Проверка, что пользователи добавлены в группу "developers"
 echo "Группы для user1:"
-
+groups user1
 
 echo "Группы для user2:"
+groups user2
