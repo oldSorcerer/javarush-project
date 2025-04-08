743728390e2ac028999216ff7f4eb51a33d3a198
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-04-08 16:48:25 +0300

    add solution system linux level02 task 27

--- src/ru/javarush/system/linux/level02/task27/solution.sh
+++ src/ru/javarush/system/linux/level02/task27/solution.sh
@@ -1,10 +1,11 @@
 # Создаём файл project.txt в домашней директории пользователя user1
-
+sudo -u user1 touch /home/user1/project.txt
 
 # Меняем владельца файла на пользователя user1, а группу на developers
-
+sudo chown user1:developers /home/user1/project.txt
 
 # Устанавливаем права доступа: только владелец и члены группы могут читать и записывать файл
-
+sudo chmod 660 /home/user1/project.txt
 
 # Проверяем итоговые права доступа на файл
+ls -l /home/user1/project.txt
