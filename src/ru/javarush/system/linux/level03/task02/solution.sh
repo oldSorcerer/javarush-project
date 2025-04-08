d7fc0352165af344d725004c0654aa762fc08e9c
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-04-08 16:49:10 +0300

    add solution system linux level03 task 02

--- src/ru/javarush/system/linux/level03/task02/solution.sh
+++ src/ru/javarush/system/linux/level03/task02/solution.sh
@@ -1,7 +1,8 @@
 # Проверка статуса службы 'cron'
-
+sudo systemctl status cron
 
 # Включение автозапуска для службы 'cron'
-
+sudo systemctl enable cron
 
 # Проверка, включён ли автозапуск для службы 'cron'
+sudo systemctl is-enabled cron
