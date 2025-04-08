c3dd2f110b5a6e87479575b57faeb846fd5c9a41
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-04-08 16:49:05 +0300

    add solution system linux level03 task 01

--- src/ru/javarush/system/linux/level03/task01/solution.sh
+++ src/ru/javarush/system/linux/level03/task01/solution.sh
@@ -3,9 +3,10 @@ sudo apt-get update
 sudo apt-get install -y cron
 
 # Запускаем службу cron
-
+sudo systemctl start cron
 
 # Останавливаем службу cron
-
+sudo systemctl stop cron
 
 # Проверяем статус службы cron
+sudo systemctl status cron
