0efd95710f0861f21658106e0a00a12256e08680
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-04-10 02:01:34 +0300

    add solution system linux level05 task 01

--- src/ru/javarush/system/linux/level03/task03/solution.sh
+++ src/ru/javarush/system/linux/level03/task03/solution.sh
@@ -5,9 +5,10 @@ sudo systemctl restart cron
 sudo systemctl status cron
 
 # Остановка службы cron
-
+sudo systemctl stop cron
 
 # Проверка всех завершённых с ошибкой служб
-
+sudo systemctl --failed
 
 # Проверка состояния остановленной службы cron
+sudo systemctl status cron
--- src/ru/javarush/system/linux/level03/task04/solution.sh
+++ src/ru/javarush/system/linux/level03/task04/solution.sh
@@ -3,13 +3,13 @@ sudo apt update
 sudo apt install -y nginx
 
 # Запуск службы nginx
-
+sudo systemctl start nginx
 
 # Перезапуск службы nginx
-
+sudo systemctl restart nginx
 
 # Проверка статуса службы nginx
-
+sudo systemctl status nginx
 
 # Отключение автозапуска службы nginx
 
