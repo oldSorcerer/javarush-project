0d60200fa90f56b37b80e05968d5d3b1a491e329
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-04-14 11:58:39 +0300

    add solution system linux level03 task 11

--- src/ru/javarush/system/linux/level03/task11/solution.sh
+++ src/ru/javarush/system/linux/level03/task11/solution.sh
@@ -1,11 +1,11 @@
 # Устанавливаем временную зону на UTC
-???
+sudo timedatectl set-timezone UTC
 
 # Проверяем, что временная зона изменилась на UTC
-??? | grep "Time zone"
+timedatectl status | grep "Time zone"
 
 # Устанавливаем временную зону обратно на Europe/Moscow
-??? Europe/Moscow
+sudo timedatectl set-timezone Europe/Moscow
 
 # Проверяем, что временная зона изменилась обратно на Europe/Moscow
-??? | grep "Time zone"
+timedatectl status | grep "Time zone"
