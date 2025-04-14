4ee404ebfd3f751608e9e0d0a80df4469542c3e0
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-04-14 11:58:31 +0300

    add solution system linux level03 task 10

--- src/ru/javarush/system/linux/level03/task10/solution.sh
+++ src/ru/javarush/system/linux/level03/task10/solution.sh
@@ -1,6 +1,6 @@
 # Выводим список всех доступных временных зон
-???
+timedatectl list-timezones
 
 # Определяем временную зону для города Нью-Йорк
 # Используем фильтрацию вывода команды для нахождения нужной временной зоны
-??? | grep -w "America/New_York"
+timedatectl list-timezones | grep -w "America/New_York"
