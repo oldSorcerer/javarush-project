d46640ee1058d0b4c61d91399e2712fa8a587854
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-04-25 12:31:16 +0300

    add solution linux level04 task 13

--- src/ru/javarush/system/linux/level04/task13/solution.sh
+++ src/ru/javarush/system/linux/level04/task13/solution.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Установка SSH-клиента
-
+sudo apt-get update
+sudo apt-get install openssh-client
 
 # Проверяем текущую версию установленного SSH-клиента и выводим ее на экран
 echo "Текущая версия SSH-клиента:"
+ssh -V
