575d16d6871cc82c2fc9308783bf0861d3f2bc3c
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-04-25 12:30:46 +0300

    add solution linux level04 task 08

--- src/ru/javarush/system/linux/level04/task08/solution.sh
+++ src/ru/javarush/system/linux/level04/task08/solution.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # Добавление статического маршрута для подсети 10.10.20.0/24 через шлюз 192.168.1.254 и интерфейс eth0
-
+sudo ip route add 10.10.20.0/24 via 192.168.1.254 dev eth0
 
 # Удаление ранее добавленного маршрута
-
+sudo ip route del 10.10.20.0/24 via 192.168.1.254
 
 # Вывод списка всех портов в состоянии "Listening" и сохранение результата в файл listening_ports.txt
+ss -lnt  > listening_ports.txt
+#ss -lnt | awk '{print $1, $4, $5}' > listening_ports.txt
