463eab69f7b1fe946b08ec900eb7ec089281d311
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-05-11 12:22:06 +0300

    add solution linux level04 task 21

--- src/ru/javarush/system/linux/level04/task21/solution.sh
+++ src/ru/javarush/system/linux/level04/task21/solution.sh
@@ -5,17 +5,17 @@ sudo apt update
 sudo apt install -y openssh-server
 
 # Проверка статуса SSH-сервера
-??? | grep "Active: active (running)" > /dev/null
+sudo systemctl status ssh | grep "Active: active (running)" > /dev/null
 
 if [ $? -eq 0 ]; then
   echo "SSH-сервер активен."
 else
   echo "Запуск SSH-сервера..."
-  ???
+  sudo systemctl start ssh
 fi
 
 # Перезапуск SSH-сервера
-???
+sudo systemctl restart ssh
 
 # Финальная проверка
-??? | grep "Active: active (running)" > /dev/null && echo "SSH-сервер успешно работает." || echo "Ошибка: SSH-сервер не запущен."
+sudo systemctl status ssh | grep "Active: active (running)" > /dev/null && echo "SSH-сервер успешно работает." || echo "Ошибка: SSH-сервер не запущен."
