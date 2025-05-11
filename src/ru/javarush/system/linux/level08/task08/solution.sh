f1ad41e4a1708d27f52a9f54c0a4be05221d3402
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-05-11 12:23:08 +0300

    add solution linux level08 task 08

--- src/ru/javarush/system/linux/level08/task08/solution.sh
+++ src/ru/javarush/system/linux/level08/task08/solution.sh
@@ -2,7 +2,7 @@
 
 # Используем команду sed с регулярным выражением для замены строк,
 # начинающихся с "error" на "Issue Detected"
-???
+sed -E 's/^error:.*/Issue Detected/' logs.txt > updated_logs.txt
 
 # Перезаписываем исходный файл обновленным содержимым
 mv updated_logs.txt logs.txt
