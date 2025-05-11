360482f54b0c206ec29a3ae37f3e844f1ec79eb8
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-05-11 12:22:10 +0300

    add solution linux level04 task 22

--- src/ru/javarush/system/linux/level04/task22/solution.sh
+++ src/ru/javarush/system/linux/level04/task22/solution.sh
@@ -5,14 +5,14 @@ sudo apt update
 sudo apt install -y openssh-server
 
 # Включаем и запускаем SSH-сервер
-???
-???
+sudo systemctl enable ssh
+sudo systemctl start ssh
 
 # Проверяем статус SSH-сервера, чтобы убедиться, что он работает
-???
+sudo systemctl status ssh
 
 # Получаем имя текущего пользователя
-???
+CURRENT_USER=$(whoami)
 
 # Подключение к локальному SSH-серверу
 echo "Подключение к SSH-серверу на 127.0.0.1 от имени пользователя $CURRENT_USER..."
