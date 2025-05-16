5e37d2160642014cdc2b6e5bbe29bbd9db691b76
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-05-16 11:19:09 +0300

    add solution linux level08 task 18

--- src/ru/javarush/system/linux/level08/task18/solution.sh
+++ src/ru/javarush/system/linux/level08/task18/solution.sh
@@ -1,11 +1,13 @@
-#!/bin/bash
+ #!/bin/bash
 
 # URL изображения, которое требуется скачать
-IMAGE_URL="https://via.placeholder.com/150.jpg"
+#IMAGE_URL="https://via.placeholder.com/150.jpg"
+IMAGE_URL="https://placehold.co/150x150.jpg"
 
 # Скачивание изображения с использованием wget
 # -q используется для тихого режима, а --show-progress для отображения прогресса
-???
+#wget -q --show-progress "$IMAGE_URL"
+wget -O 150x150.jpg "$IMAGE_URL"
 
 # Проверка успешности выполнения команды wget
 if [ $? -eq 0 ]; then
@@ -13,3 +15,5 @@ if [ $? -eq 0 ]; then
 else
     echo "Ошибка при скачивании файла."
 fi
+
+sleep 20
