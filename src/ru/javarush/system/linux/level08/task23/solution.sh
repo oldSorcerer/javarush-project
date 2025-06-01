e0372a2326a92853e36447c5d9e0864aac90287f
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-06-01 11:00:06 +0300

    add solution linux level08 task 23

--- src/ru/javarush/system/linux/level08/task23/solution.sh
+++ src/ru/javarush/system/linux/level08/task23/solution.sh
@@ -4,6 +4,7 @@
 sudo apt-get update
 
 # Полное обновление системы, включая все пакеты и их зависимости
-
+sudo apt-get dist-upgrade -y
 
 # Удаление всех неиспользуемых пакетов и зависимостей
+sudo apt-get autoremove -y
