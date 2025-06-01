145354a982a61f7e19bd6d39c0d8aac5f889d73e
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-06-01 11:00:20 +0300

    add solution linux level08 task 24

--- src/ru/javarush/system/linux/level08/task24/solution.sh
+++ src/ru/javarush/system/linux/level08/task24/solution.sh
@@ -1,13 +1,15 @@
 # Для Debian/Ubuntu
 echo "Debian/Ubuntu:"
-
+sudo apt-get update
+sudo apt-get install -y vim
 
 echo "Версия vim на Debian/Ubuntu:"
-
+vim --version | head -n 1
 
 # Для CentOS/Fedora
 echo "CentOS/Fedora:"
-
+sudo yum check-update
+sudo yum install -y vim
 
 echo "Версия vim на CentOS/Fedora:"
-
+vim --version | head -n 1
