3cca01f505e7b4362a1cbb30fe5b251c3a37099e
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-06-01 14:40:09 +0300

    add solution docker level01 task 11

--- src/ru/javarush/python/fullstack/level01/task11/solution.sh
+++ src/ru/javarush/python/fullstack/level01/task11/solution.sh
@@ -1,7 +1,11 @@
 # Создание новой сети с именем app_network
+docker network create app_network
 
 # Подключение контейнера с именем app_container к сети app_network
+docker network connect app_network app_container
 
 # Вывод списка всех доступных сетей
+docker network ls
 
 # Отключение контейнера app_container от сети app_network
+docker network disconnect app_network app_container
