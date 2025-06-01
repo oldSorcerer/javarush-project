9d252ec89dd3cbcbf0c24de0c086073316ba1f5b
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-06-01 14:39:59 +0300

    add solution docker level01 task 09

--- src/ru/javarush/python/fullstack/level01/task09/solution.sh
+++ src/ru/javarush/python/fullstack/level01/task09/solution.sh
@@ -1,11 +1,17 @@
 # Запуск контейнера с образом nginx в фоновом режиме с именем web_container
+docker run -d --name web_container nginx
 
 # Вывод списка запущенных контейнеров
+docker ps
 
 # Остановка контейнера web_container
+docker stop web_container
 
 # Перезапуск контейнера web_container
+docker restart web_container
 
 # Остановка контейнера web_container перед удалением
+docker stop web_container
 
 # Удаление контейнера web_container
+docker rm web_container
