d6e2ccad025deadd46ed20739a2dc7b447da8412
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-06-01 11:01:02 +0300

    add solution linux level08 task 27

--- src/ru/javarush/system/linux/level08/task27/solution.sh
+++ src/ru/javarush/system/linux/level08/task27/solution.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Загрузка файла data.log с использованием команды curl
-???
+curl -o data.log http://example.com/logs/data.log
 
 # Поиск строк, содержащих слово "WARNING", и сохранение их в файл warnings.log
-???
+grep "WARNING" data.log > warnings.log
 
 # Извлечение даты и сообщения из строк с предупреждениями и сохранение их в файл warnings_formatted.log
 # Предполагается, что формат строки в warnings.log: "YYYY-MM-DD HH:MM:SS WARNING сообщение"
-???
+awk '{print $1, substr($0, index($0, $4))}' warnings.log > warnings_formatted.log
