0c707c3bef10bbf55fcb8e1277d4216e19f6ccee
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-06-03 14:41:33 +0300

    add solution docker level01 task 14

--- src/ru/javarush/python/fullstack/level01/task14/solution.sh
+++ src/ru/javarush/python/fullstack/level01/task14/solution.sh
@@ -1,8 +1,8 @@
 # 1. Поиск образа nginx на Docker Hub
-
+docker search nginx
 
 # 2. Просмотр деталей образа nginx
 # Перейдите на https://hub.docker.com/_/nginx для просмотра деталей
 
 # 3.Загрузка последней версии образа nginx на локальную машину
-
+docker pull nginx:latest
