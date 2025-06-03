8dfcb1f5a3e54a1dbc8b83a615eb10415e674ae0
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-06-04 00:58:37 +0300

    add solution docker level02 task 19

--- src/ru/javarush/python/fullstack/level02/task19/solution.sh
+++ src/ru/javarush/python/fullstack/level02/task19/solution.sh
@@ -1,4 +1,5 @@
 # Обновление списка пакетов внутри контейнера my_container
-
+docker exec my_container apt-get update
 
 # Установка текстового редактора Vim внутри контейнера my_container
+docker exec my_container apt-get install -y vim
