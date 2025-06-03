dc78593b88653f158b0080b3be1eb3846f6798a6
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-06-03 14:41:22 +0300

    add solution docker level01 task 12

--- src/ru/javarush/python/fullstack/level01/task12/solution.sh
+++ src/ru/javarush/python/fullstack/level01/task12/solution.sh
@@ -1,7 +1,13 @@
 # Создание нового Docker тома с именем data_volume
+docker volume create data_volume
 
 # Просмотр информации о созданном томе data_volume
+docker volume inspect data_volume
 
 # Запуск контейнера и монтирование тома data_volume по пути /data
+docker run -d --name my_container -v data_volume:/data alpine
+#docker run -d --name my_container -v data_volume:/data busybox
 
 #Удаление контейнера my_container и тома data_volume
+docker rm -f my_container
+docker volume rm data_volume
