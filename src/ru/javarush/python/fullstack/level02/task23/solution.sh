f5b36cd410a5858ff946d14c601f076048bcdde0
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-06-05 00:59:13 +0300

    add solution docker level02 task 23

--- src/ru/javarush/python/fullstack/level02/task23/solution.sh
+++ src/ru/javarush/python/fullstack/level02/task23/solution.sh
@@ -1,1 +1,2 @@
 # Запустите контейнер с образом nginx, ограничив использование 512 MB оперативной памяти и 512 MB подкачки (swap).
+docker run -d --name swap_limited_container --memory 512m --memory-swap 1024m nginx
