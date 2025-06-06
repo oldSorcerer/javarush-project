47a51dd8137ee2f91eb85ac734394277cf737fc1
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-06-06 21:31:18 +0300

    add solution docker level02 task 33

--- src/ru/javarush/python/fullstack/level02/task33/solution.sh
+++ src/ru/javarush/python/fullstack/level02/task33/solution.sh
@@ -1,4 +1,5 @@
 # Команда для загрузки последнего официального образа Nginx с Docker Hub
-
+docker pull nginx:latest
 
 # Команда для проверки наличия загруженного образа
+docker images | grep nginx
