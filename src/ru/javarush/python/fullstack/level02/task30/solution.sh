9e3852f1309a0aa372fedc455d1cfe554e9b7b90
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-06-08 01:00:18 +0300

    add solution docker level02 task 30

--- src/ru/javarush/python/fullstack/level02/task30/solution.sh
+++ src/ru/javarush/python/fullstack/level02/task30/solution.sh
@@ -1,7 +1,8 @@
 # Создайте именованный том:
-
+docker volume create shared_volume
 
 # Запустите первый контейнер:
-
+docker run -d --name container_one -v shared_volume:/shared nginx
 
 # Запустите второй контейнер:
+docker run -d --name container_two -v shared_volume:/shared nginx
