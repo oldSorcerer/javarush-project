2cf14e0105d383a66dc094e77b8b212a48d7f154
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-06-08 01:00:12 +0300

    add solution docker level02 task 29

--- src/ru/javarush/python/fullstack/level02/task29/solution.sh
+++ src/ru/javarush/python/fullstack/level02/task29/solution.sh
@@ -1,1 +1,2 @@
 # Запустите контейнер на основе образа nginx, смонтировав директорию /host/project на хосте в директорию /app внутри контейнера.
+docker run -d --name project_container -v /host/project:/app nginx
