4e8b354e28252dc6e3196c5de5f9fa8b79d175eb
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-06-15 10:56:29 +0300

    add solution docker level02 task 31

--- src/ru/javarush/python/fullstack/level02/task31/solution.sh
+++ src/ru/javarush/python/fullstack/level02/task31/solution.sh
@@ -2,7 +2,18 @@
 mkdir -p /backup
 
 # Создаем резервную копию тома my_volume, используя контейнер busybox
-
+docker run --rm -v my_volume:/volume -v /backup:/backup busybox tar czf /backup/my_volume_backup.tar.gz -C /volume .
 
 # Восстанавливаем том my_volume из ранее созданной резервной копии
+docker run --rm -v my_volume:/volume -v /backup:/backup busybox sh -c "tar xzf /backup/my_volume_backup.tar.gz -C /volume"
 
+# c — создать новый архив (create).
+# . — добавлять в архив всё содержимое текущей директории (т.е. весь том)
+
+# sh — это команда для запуска шелла
+# -c — указывает шёллу выполнить строку, которую вы помещаете в кавычки.
+# x — извлечение файлов (extract)
+# z — использовать gzip (архив .gz)
+# f — указывает, что далее будет имя файла архива.
+# -C /volume — говорит tar распаковать содержимое архива в каталог /volume.
+#
