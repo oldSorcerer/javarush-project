20482027cb30d1fb801f00e987bd6468a57ce5d0
oldSorcerer <aleksei.derevyannykh@gmail.com>
2025-06-25 21:04:14 +0300

    add solution docker level01 task 12

--- src/ru/javarush/python/fullstack/level01/task12/solution.sh
+++ src/ru/javarush/python/fullstack/level01/task12/solution.sh
@@ -11,3 +11,5 @@ docker run -d --name my_container -v data_volume:/data alpine
 #Удаление контейнера my_container и тома data_volume
 docker rm -f my_container
 docker volume rm data_volume
+
+# -f позволяет принудительно остановить работающий контейнер и затем его удалить
